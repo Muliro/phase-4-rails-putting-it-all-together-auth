66fcdd764aa8e1ccf6a8eb412a95e999a4263d9f
muliro <mulirokhaemba@gmail.com>
2022-10-13 00:12:23 +0300

    commit tree 9c32f942b6bd312a3e29be5467b12daa6b3f2399

---
+++ app/models/recipe.rb
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+class Recipe < ApplicationRecord
+    belongs_to :user
+    validates :title, presence: true
+    validates :instructions, presence: true,  length: { minimum: 50 }
+end
---
+++ db/migrate/20221005125535_add_user_id_to_recipe.rb
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+class AddUserIdToRecipe < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :recipes, :user, foreign_key: true
+  end
+end
